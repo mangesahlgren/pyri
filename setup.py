c73676c2139c48f94bcbd659a574a2c80fda7a3b
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2016-05-09 16:17:03 +0200

    just shuffling around

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup
+import murmurhash
 
-setup(
-    name='python random indexing',
-    version='0.1',
-    descritption='python module for random indexing and other dsms',
-    url='http://github.com/mangerabbit/pyri',
-    author='Magnus Sahlgren',
-    author_email='mange@gavagai.se',
-    license='MIT',
-    packages=['pyri'],
-    install_requires[
-        'numpy',
-        'scipy',
-        ],
-    zip_safe=False
-    )
+
