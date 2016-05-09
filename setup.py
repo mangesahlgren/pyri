e04eba516137b73d19df6acaef447a6ae4fdd900
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2016-05-09 13:57:48 +0200

    setuptools stuff

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python random indexing',
+    version='0.1',
+    descritption='python module for random indexing and other dsms',
+    url='http://github.com/mangerabbit/pyri',
+    author='Magnus Sahlgren',
+    author_email='mange@gavagai.se',
+    license='MIT',
+    packages=['pyri'],
+    install_requires[
+        'numpy',
+        'scipy',
+        ],
+    zip_safe=False
+    )
