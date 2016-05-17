fixed setup.py, added variance pruning, fixed typos in notes

pyri/ri.py
@@ -5,7 +5,9 @@ re-implementation April 2016
 import math
 from collections import Counter
 from enum import Enum
+import numpy as np
 import pyri.util as util
+
 
 def words(line):
     """
@@ -13,12 +15,14 @@ def words(line):
     """
     yield from iter(line.strip().split())
 
+
 class Direction(Enum):
     """
     An enum encoding direction: left, or right.
     """
     left = 0
     right = 1
+
 
 class WordSpace(object):
     """
@@ -30,10 +34,10 @@ class WordSpace(object):
         self.projection = projection
         self.mincount = mincount
         self.theta = theta
-        self.total = 0             # Total number of tokens
-        self.collocation = {}      # focus o=> (context counter)
-        self.wordcount = Counter() # focus counter
-        self.vecs = {}             # Dense vectors
+        self.total = 0              # Total number of tokens
+        self.collocation = {}       # focus o=> (context counter)
+        self.wordcount = Counter()  # focus counter
+        self.vecs = {}              # Dense vectors
 
 # I'd prefer the following, as it is semantically simpler.
 # Following the structure of the old code, the other variant should be used.
@@ -54,9 +58,9 @@ class WordSpace(object):
             self.collocation[focus] = Counter()
         for context in contexts:
             self.collocation[focus][(Direction.left, context)] += \
-                    self.get_weight(context)
+                self.get_weight(context)
             self.collocation[context][(Direction.right, focus)] += \
-                    self.get_weight(focus)
+                self.get_weight(focus)
 
     def get_weight(self, word):
         """
@@ -83,6 +87,7 @@ class WordSpace(object):
                     self.vecs[focus] += self.projection(context) * weight
                 del self.collocation[focus]
 
+
 def dsm(infile, size, wordspace):
     """
     Add the contents of infile to the wordspace,
@@ -94,59 +99,31 @@ def dsm(infile, size, wordspace):
                 wordspace.add_counts(focus, left_context)
     return wordspace
 
+
+def prune_high_variance(row_vecs, nrstd):
+    """
+    Remove columns with high variance: the columns whose
+    variance deviates more than nrstd standard deviations
+    from the mean (of all column variances).
+    """
+    varvec = np.var(row_vecs, 0)
+    varmean = np.mean(varvec)
+    varstd = np.std(varvec)
+    return row_vecs * (np.absolute(varvec - varmean) < varstd * nrstd)
+
+
+def prune_low_variance(row_vecs, nrstd):
+    """
+    Remove columns with low variance: the columns whose
+    variance deviates less than nrstd standard deviations
+    from the mean (of all column variances).
+    """
+    varvec = np.var(row_vecs, 0)
+    varmean = np.mean(varvec)
+    varstd = np.std(varvec)
+    return row_vecs * (np.absolute(varvec - varmean) > varstd * nrstd)
+
 """
-np.seterr(divide='ignore', invalid='ignore')
-
-####################################
-# Global variables
-####################################
-
-dimen = 2000
-nonzeros = 8
-delta = 60
-theta = 0.5
-
-####################################
-# Core functions
-####################################
-
-def check_reps(wrd,wordtypes):
-    global rivecs
-    global distvecs
-    global worddict
-    if wrd in worddict:
-        return 0
-    else:
-        rivecs.append(make_index())
-        distvecs.append(np.zeros(dimen))
-        worddict[wrd] = [wordtypes,0]
-        return 1
-
-def update_vecs(wrdlst,win,wordtokens,wordtypes):
-    global distvecs
-    global worddict
-    localtoken = 0
-    ind = 0
-    stop = len(wrdlst)
-    for w in wrdlst:
-        localtoken += 1
-        wordtypes += check_reps(w,wordtypes)
-        wind = worddict[w][0]
-        wvec = distvecs[wind]
-        wri = rivecs[wind]
-        worddict[w][1] += 1
-        lind = 1
-        while (lind < win+1) and ((ind+lind) < stop):
-            c = wrdlst[ind+lind]
-            wordtypes += check_reps(c,wordtypes)
-            cind = worddict[c][0]
-            cvec = distvecs[cind]
-            np.add.at(wvec,rivecs[cind][:,0]+1,rivecs[cind][:,1]*weight_func(worddict[c][1],wordtypes))
-            np.add.at(cvec,rivecs[wind][:,0]-1,rivecs[wind][:,1]*weight_func(worddict[w][1],wordtypes))
-            lind += 1
-        ind += 1
-    return localtoken, wordtypes
-
 def update_vecs_ngrams(wrdlst,win,wordtokens,wordtypes):
     global distvecs
     global worddict
@@ -172,14 +149,6 @@ def update_vecs_ngrams(wrdlst,win,wordtokens,wordtypes):
         ind += 1
     return localtoken, wordtypes
 
-def make_index():
-    ret = []
-    inds = nprnd.randint(dimen-2, size=nonzeros) # dimen-2 to facilitate directional permutation
-    for i in inds:
-        sign = nprnd.randint(0,2)*2-1
-        ret.append([i+1, sign]) # +1 to facilitate directional permutation
-    return np.array(ret)
-
 # TODO
 # def make_very_sparse_index():
 
@@ -187,51 +156,8 @@ def make_index():
 # Vector operations
 ####################################
 
-# online frequency weight defined in:
-# Sahlgren et al. (2016) The Gavagai Living Lexicon, LREC
-def weight_func(freq,words):
-    global delta
-    return math.exp(-delta*(freq/words))
-
-# remove dimensions with high variance
-def prune_high_var(nrstd):
-    global distvecs
-    tmpmat = np.asmatrix(distvecs)
-    varvec = np.var(tmpmat,0)
-    varmean = np.mean(varvec)
-    varstd = np.std(varvec)
-    prunevec = np.zeros(len(distvecs[0]))
-    ind = 0
-    for i in np.nditer(varvec):
-        if (i > (varmean + (varstd * nrstd))) or (i < (varmean - (varstd * nrstd))):
-            prunevec[ind] = 0.0
-        else:
-            prunevec[ind] = 1.0
-        ind += 1
-    cnt = 0
-    for d in distvecs:
-        distvecs[cnt] = d*prunevec
-        cnt += 1
 
 # remove dimensions with low variance
-def prune_low_var(nrstd):
-    global distvecs
-    tmpmat = np.asmatrix(distvecs)
-    varvec = np.var(tmpmat,0)
-    varmean = np.mean(varvec)
-    varstd = np.std(varvec)
-    prunevec = np.zeros(len(distvecs[0]))
-    ind = 0
-    for i in np.nditer(varvec):
-        if (i < (varmean + (varstd * nrstd))) and (i > (varmean - (varstd * nrstd))):
-            prunevec[ind] = 1.0
-        else:
-            prunevec[ind] = 0.0
-        ind += 1
-    cnt = 0
-    for d in distvecs:
-        distvecs[cnt] = d*prunevec
-        cnt += 1
 
 def isvd(upper,lower):
     global distvecs